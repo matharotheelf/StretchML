a1815172cc87409f86115d72b16651b87450a184
Thomas Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2024-10-03 23:43:44 +0200

    comment description of data extraction code

--- stretchPoseData.js
+++ stretchPoseData.js
@@ -9,6 +9,8 @@ class StretchPoseData {
   #rightHandPosition
 
   constructor(keypoints) {
+    // extract the keypoint data needed for classifying the upper body stretches
+    // save them to vector objects for manipulation
     this.#nosePosition = createVector(keypoints[0].x, keypoints[0].y);
     this.#leftShoulderPosition = createVector(keypoints[5].x, keypoints[5].y);
     this.#rightShoulderPosition = createVector(keypoints[6].x, keypoints[6].y);
@@ -17,7 +19,11 @@ class StretchPoseData {
     this.#leftHandPosition = createVector(keypoints[9].x, keypoints[9].y)
     this.#rightHandPosition = createVector(keypoints[10].x, keypoints[10].y)
 
+    // calculate the midpoint between the shoulders
+    // this is used to calculate the head displacement from the shoulders
     this.#midShoulderPosition = this.calculateMidShoulderPosition()
+
+    // calculate all the displacement vectors which are used in the stretch classification process 
 
     this.headDisplacement = this.calculateHeadDisplacement(keypoints);
 
@@ -29,29 +35,36 @@ class StretchPoseData {
   }
   
   calculateMidShoulderPosition() {
+    // average position of the two shoulder keypoints
     return p5.Vector.add(p5.Vector.sub(this.#rightShoulderPosition, this.#leftShoulderPosition).div(2), this.#leftShoulderPosition)
   }
 
   calculateHeadDisplacement() {
+    // dispacement between nose and shoulder
     return p5.Vector.sub(this.#nosePosition, this.#midShoulderPosition);
   }
 
   calculateLeftUpperArmDisplacement() {
+    // displacement between elbow and shoulder
     return p5.Vector.sub(this.#leftElbowPosition, this.#leftShoulderPosition);
   }
 
   calculateLeftLowerArmDisplacement() {
+    // displacement between hand and elbow
     return p5.Vector.sub(this.#leftHandPosition, this.#leftElbowPosition);
   }
 
   calculateRightUpperArmDisplacement() {
+    // displacement between shoulder and elbow
     return p5.Vector.sub(this.#rightElbowPosition, this.#rightShoulderPosition);
   }
 
   calculateRightLowerArmDisplacement() {
+    // displacement between elbow and hand
     return p5.Vector.sub(this.#rightHandPosition, this.#rightElbowPosition);
   }
 
+  // visualise all of the extracted displacement vectors
   draw() {
     push();
     translate(width / 2, height / 2);
